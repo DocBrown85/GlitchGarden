988a42a16dff436a1c621020533aa94f1804e2a2
docbrown85 <me@example.com>
2020-05-14 11:44:09 +0200

    Add defender shoot only if there are attackers in the same lane

--- GlitchGarden/Assets/Scripts/Shooter.cs
+++ GlitchGarden/Assets/Scripts/Shooter.cs
@@ -7,6 +7,50 @@ public class Shooter : MonoBehaviour
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
 
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("SHOOT!");
+        }
+        else
+        {
+            Debug.Log("WAIT");
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+        foreach(AttackerSpawner attackerSpawner in attackerSpawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(attackerSpawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = attackerSpawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, transform.rotation);
